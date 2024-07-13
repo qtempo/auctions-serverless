947620e97cc9805098fe21e25cc4c4d21838ef6d
yaroslavprt <yaroslavprt@gmail.com>
2024-07-13 19:38:45 +0300

    feat: refactor middleware

--- auctions-service/src/utils/middleware.ts
+++ auctions-service/src/utils/middleware.ts
@@ -1,5 +1,5 @@
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context, Handler } from 'aws-lambda'
 import { Utils } from './utils'
 
 /**
@@ -26,9 +26,8 @@ interface AuctionsLambdaHandler<B, P, Q> {
   (event: APIGatewayProxyEventParsed<B, P, Q>, context: Context): Promise<APIGatewayProxyResult>
 }
 
-interface AuctionsMiddleware<B, P, Q> {
-  (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult>
-  handler: (handler: AuctionsLambdaHandler<B, P, Q>) => AuctionsMiddleware<B, P, Q>
+interface LambdaHandler<B, P, Q> extends Handler<APIGatewayProxyEvent, APIGatewayProxyResult> {
+  handler: (handler: AuctionsLambdaHandler<B, P, Q>) => LambdaHandler<B, P, Q>
 }
 
 /**
@@ -38,7 +37,7 @@ interface AuctionsMiddleware<B, P, Q> {
 export const middleware = <B, P, Q>() => {
   let auctionLambda: AuctionsLambdaHandler<B, P, Q>
 
-  const middlewareHandler: AuctionsMiddleware<B, P, Q> = async (event, context) => {
+  const middlewareHandler: LambdaHandler<B, P, Q> = async (event, context) => {
     try {
       const eventBody: EventBody<B> = {
         body: JSON.parse(event.body ?? '{}') as B,
